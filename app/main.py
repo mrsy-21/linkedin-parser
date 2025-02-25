3f76d4a271ec28fbadf81aac5f5f15af78e9bfb7
Oleksandr-Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2025-02-25 20:22:56 +0000

    Оновлено requirements.txt

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ class Parser:
     """
 
     headers_dict = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
